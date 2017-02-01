b1cafa9344a5055b46599a8d296c6c47eb1a3d28
Jonathan Caryl <jonathan.caryl@livingsocial.com>
2017-02-01 15:48:13 +0000

    Timber

--- app/src/main/java/jonathancaryl/org/base/BaseApplication.java
+++ app/src/main/java/jonathancaryl/org/base/BaseApplication.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import jonathancaryl.org.base.components.ApplicationComponent;
 import jonathancaryl.org.base.components.DaggerApplicationComponent;
 import jonathancaryl.org.base.modules.ApplicationModule;
+import timber.log.Timber;
 
 public class BaseApplication extends Application {
     protected ApplicationComponent applicationComponent;
@@ -17,6 +18,11 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        // Ideally we would inject a suitable tree for planting,
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+
         applicationComponent = DaggerApplicationComponent
                 .builder()
                 .applicationModule(new ApplicationModule(this))
