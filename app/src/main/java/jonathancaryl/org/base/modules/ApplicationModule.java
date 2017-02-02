551fc3edd022ff9ebbb6acdbe24640f3510396d2
Jonathan Caryl <jonathan.caryl@livingsocial.com>
2017-02-02 09:14:18 +0000

    Retrofit

--- app/src/main/java/jonathancaryl/org/base/modules/ApplicationModule.java
+++ app/src/main/java/jonathancaryl/org/base/modules/ApplicationModule.java
@@ -5,16 +5,24 @@ import android.content.Context;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.jakewharton.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 import jonathancaryl.org.base.annotations.ApplicationContext;
+import okhttp3.Cache;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 @Module
 public class ApplicationModule {
+    private static final int DISK_CACHE_SIZE = 50 * 1024 * 1024;
 
     private final Application mApplication;
 
@@ -35,8 +43,35 @@ public class ApplicationModule {
 
     @Provides
     @Singleton
-    Picasso providePicasso(@ApplicationContext Context context) {
-        return Picasso.with(context);
+    Cache provideOkHttpCache(Application application) {
+        return new Cache(new File(application.getCacheDir(), "http"), DISK_CACHE_SIZE);
+    }
+
+    @Provides
+    @Singleton
+    Interceptor provideInterceptor() {
+        return chain -> {
+            Request.Builder builder = chain.request().newBuilder();
+            builder.addHeader("Accept", "application/json");
+            return chain.proceed(builder.build());
+        };
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient(Cache cache, Interceptor interceptor) {
+        return new OkHttpClient.Builder()
+                .cache(cache)
+                .addNetworkInterceptor(interceptor)
+                .build();
+    }
+
+    @Provides
+    @Singleton
+    Picasso providePicasso(@ApplicationContext Context context, OkHttpClient client) {
+        return new Picasso.Builder(context)
+                .downloader(new OkHttp3Downloader(client))
+                .build();
     }
 
     @Provides
